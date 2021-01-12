027ea2a5f2c46f902f974596fe786f4e832fa2fc
dylanaldrich <aldrich.dylan@gmail.com>
2021-01-12 16:21:18 -0500

    adds basic views

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def api(request):
+    return JsonResponse({"status": 200})
+
+def bios(request):
+    return render(request, 'bios.html')
+
+def episode_detail(request, episode_id):
+    episode = Episode.objects.get(id=episode_id)
+    return render(request, 'episode.html', episode)
