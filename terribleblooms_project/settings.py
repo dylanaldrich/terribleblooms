d2103845ec97959bec03d65bc337abc92194d573
dylanaldrich <aldrich.dylan@gmail.com>
2021-03-12 15:32:12 -0500

    adjusting static settings 3

--- terribleblooms_project/settings.py
+++ terribleblooms_project/settings.py
@@ -26,7 +26,7 @@ environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname((os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -38,7 +38,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', ENV_SECRET)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
