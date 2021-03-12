bf05173aff0797a41da12cdaf2116d971bb85231
dylanaldrich <aldrich.dylan@gmail.com>
2021-03-12 18:33:27 -0500

    still figuring out the static files problem

--- terribleblooms_project/settings.py
+++ terribleblooms_project/settings.py
@@ -35,9 +35,9 @@ ENV_SECRET = env('SECRET_KEY')
 SECRET_KEY = os.getenv('SECRET_KEY', ENV_SECRET)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['terribleblooms.herokuapp.com']
+ALLOWED_HOSTS = []
 
 # Application definition
 
--- terribleblooms_project/urls.py
+++ terribleblooms_project/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
 #     (r'^static/(?P.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
 # )
 
-# if settings.DEBUG:
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
