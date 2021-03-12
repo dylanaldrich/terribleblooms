43fc1277259ee2781f8042180f38e97f76b9d740
dylanaldrich <aldrich.dylan@gmail.com>
2021-03-12 14:56:25 -0500

    changes static files storage config once again

--- terribleblooms_project/settings.py
+++ terribleblooms_project/settings.py
@@ -166,8 +166,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
