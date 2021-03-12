eac9e2c12263eb63182d596ef6cfc556a316076a
dylanaldrich <aldrich.dylan@gmail.com>
2021-03-12 15:43:55 -0500

    adjusting static settings 8

--- terribleblooms_project/settings.py
+++ terribleblooms_project/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', ENV_SECRET)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 # Application definition
 
