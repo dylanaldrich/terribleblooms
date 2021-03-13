c9df040d5fd19d5f65bc273353e0311e14341895
dylanaldrich <aldrich.dylan@gmail.com>
2021-03-13 08:57:46 -0500

    trying static configs 3

--- terribleblooms_project/settings.py
+++ terribleblooms_project/settings.py
@@ -166,7 +166,7 @@ STATICFILES_DIRS = (
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
 
 # STATICFILES_FINDERS = (
 #     'django.contrib.staticfiles.finders.FileSystemFinder',
