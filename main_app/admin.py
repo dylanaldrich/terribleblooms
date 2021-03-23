5c3e89c2ec60cc5497f0ac589548198de019b639
dylanaldrich <aldrich.dylan@gmail.com>
2021-03-23 11:25:54 -0400

    edits admin classes for creator and performer

--- main_app/admin.py
+++ main_app/admin.py
@@ -4,11 +4,11 @@ from .models import Performer, Play, Creator
 # Register your models here.
 class CreatorAdmin(admin.ModelAdmin):
     fields = ('name', 'title', 'bio', 'email')
-    list_display = ['name', 'title', 'bio', 'email']
+    list_display = ['name']
 
 class PerformerAdmin(admin.ModelAdmin):
     fields = ('name', 'bio', 'imdb')
-    list_display = ['name', 'bio', 'imdb']
+    list_display = ['name']
 
 admin.site.register(Performer, PerformerAdmin)
 admin.site.register(Creator, CreatorAdmin)
